62d72d70a4132c09d66635821fb4de92fd8b6cd8
Siddhu45 <152026185+Siddhu4554@users.noreply.github.com>
2024-10-23 15:44:00 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,7 @@ export function Header() {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-center space-x-3">
           <FileText size={32} className="text-white" />
+          <img src="Logo.png" height="40" width="40">
           <h1 className="text-3xl font-bold">GitHub README Generator</h1>
         </div>
         <p className="text-center mt-4 text-purple-100 max-w-2xl mx-auto">
